40caaba9aeb8fb4943aa1a35783fa30da4eb647f
Rafael-Chaves2 <rafael.chaves@b2mlportal.com.br>
2024-02-18 13:21:08 -0300

    refactor: add tab name

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/portfolio/pages/home/home.component';
 
 export const routes: Routes = [{
+    title: 'Rafael Chaves',
     path: '',
     component: HomeComponent
 }];
